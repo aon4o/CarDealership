c955229e661441aeff3a6ef7ca0ac30775c69f23
Alex <a.o.naidenov@gmail.com>
2019-12-30 18:02:16 +0200

    The show file for customers: rewritten to work. (даже няма warning-и хД)

--- resources/views/customers/show.blade.php
+++ resources/views/customers/show.blade.php
@@ -1,30 +1,35 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
     <title>Customers SHOW</title>
 </head>
 <body>
-<a href="/">Back to Main</a></br>
-<a href="{{ route('brands.index') }}">Back to Customers</a>
+<a href="/">Back to Main</a><br>
+<a href="{{ route('customers.index') }}">Back to Customers</a>
 <h1>Customer Show</h1>
-<hr></hr>
+<hr/>
 <table>
     <thead>
     <tr>
         <th>ID</th>
-        <th>Name</th>
+        <th>First name</th>
+        <th>Last name</th>
+        <th>Date of Birth</th>
+        <th>EGN</th>
         <th>Actions</th>
     </tr>
     </thead>
     <tbody>
     <tr>
-        <td>{{ $brand->id }}</td>
-        <td>{{ $customer->name }}</a></td>
+        <td>{{ $customer->id }}</td>
+        <td>{{ $customer->first_name }}</td>
+        <td>{{ $customer->last_name }}</td>
+        <td>{{ $customer->born_at }}</td>
+        <td>{{ $customer->egn }}</td>
         <td><a href="{{ route('customers.edit', ['customer' => $customer]) }}">Edit</a></td>
         <td><form action={{ route('customers.destroy', ['customer' => $customer]) }} method="POST">
                 @csrf
                 @method('DELETE')
-                <input type='hidden' name='id' value="{{$customer->id}}">
                 <input type="submit" value="Delete"/>
             </form>
         </td>
